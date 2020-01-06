6bfc59678140e985ccb316f232fb8b480f00dbd3
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-01-06 10:41:24 +0000

    implemented check of overlap between histograms using quantiles

--- plot_histograms.py
+++ plot_histograms.py
@@ -2,6 +2,7 @@
 
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     data = {}
@@ -13,8 +14,7 @@ def main():
 
     for file in files:
         with open(file, 'r') as f:
-            file_number = file.split('_')[-1].split('.')[0]
-            print(file_number)
+            file_number = int(file.split('_')[-1].split('.')[0])
             data[file_number] = []
 
             for line in f.readlines():
@@ -24,6 +24,21 @@ def main():
             plt.hist(data[file_number], bins=100, histtype='step')
     plt.xlabel('angle')
     plt.ylabel('data points')
+
+    #  use quantiles to check proper overlap of distributions
+    for key in sorted(data.keys()):
+        key_next = key + 1
+        if key_next in data.keys():
+            dat = np.asarray(data[key])
+            dat_next = np.asarray(data[key_next])
+            dat_quantile = np.quantile(dat, 0.8)
+            dat_next_quantile = np.quantile(dat_next, 0.2)
+            plt.axvline(dat_quantile, color='red')
+            plt.axvline(dat_next_quantile)
+            if (dat_quantile > dat_next_quantile):
+                print(key, key_next, 'True')
+            else:
+                print(key, 'False')
     plt.show()
 
 
