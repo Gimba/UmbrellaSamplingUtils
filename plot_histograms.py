2db4ea16cec1273b8a3edcd1b021a4db3c2bdf23
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-01-06 09:43:57 +0000

    sort files by name

--- plot_histograms.py
+++ plot_histograms.py
@@ -3,8 +3,12 @@
 import glob
 
 def main():
+    data = {}
+    files = []
     for file in glob.glob("*.dat"):
-        print(file)
+        files.append(file)
+
+    files = sorted(files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
 
 if __name__ == '__main__':
     main()
