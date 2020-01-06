2785b36c286241813977c8f8ffadcc91145d3c5c
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-01-06 10:06:00 +0000

    copied reading in and sorting from plot_histograms

---
+++ check_overlap.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import glob
+
+
+def main():
+    data = {}
+    files = []
+    for file in glob.glob("*.dat"):
+        files.append(file)
+
+    files = sorted(files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+
+    for file in files:
+        with open(file, 'r') as f:
+            file_number = file.split('_')[-1].split('.')[0]
+            print(file_number)
+            data[file_number] = []
+
+            for line in f.readlines():
+                dat = line.split()[-1]
+                data[file_number].append(float(dat))
+
+
+if __name__ == '__main__':
+    main()
