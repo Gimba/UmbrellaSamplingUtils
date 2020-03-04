86971cf2e6bed290ed609736442181613cd15d15
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-03-04 15:26:53 +0000

    set up input parser

--- umb_dG.py
+++ umb_dG.py
@@ -1,8 +1,13 @@
 import sys
+import argparse
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='calculate delta G value between two ranges of an umbrella sampling.')
+    parser.add_argument('file', help='file containing probabilities', default='results.dat')
+    parser.add_argument('range_1', help='value range 1', default='-99,-41')
+    parser.add_argument('range_2', help='value range 2', default='1,7')
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
