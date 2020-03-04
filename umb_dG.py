c6b20730cf357439502a5089749560727554b8fa
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-03-04 15:42:39 +0000

    read in data

--- umb_dG.py
+++ umb_dG.py
@@ -1,14 +1,23 @@
 import sys
 import argparse
+import pandas as pd
 
 
-def main():
+def main(args):
     parser = argparse.ArgumentParser(description='calculate delta G value between two ranges of an umbrella sampling.')
-    parser.add_argument('file', help='file containing probabilities', default='results.dat')
-    parser.add_argument('range_1', help='value range 1', default='-99,-41')
-    parser.add_argument('range_2', help='value range 2', default='1,7')
+    parser.add_argument('-file', help='file containing probabilities', default='result.dat', required=False)
+    parser.add_argument('-range_1', help='value range 1', default='-99,-41', required=False)
+    parser.add_argument('-range_2', help='value range 2', default='1,7', required=False)
     args = parser.parse_args()
+
+    data = pd.read_csv(args.file, sep='\t')
+    data = data[~data['#Coor'].str.contains("#")]
+
+    data = data[['#Coor', '+/-']]
+
+    print(data.head())
+    print(data.tail())
 
 
 if __name__ == '__main__':
-    main(sys.args)
+    main(sys.argv)
